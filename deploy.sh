1c5e9e019264233402644df8c83b87948a37743b
Jorge Raze <jorge.alcaraz@ibm.com>
2021-06-18 20:34:34 -0500

    V1

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-export S3_BUCKET_NAME="raze-website"
-export CF_DISTRIBUTION_ID="ES52SN9381PAQ"
-export DEPLOY_FOLDER="./public"
+export S3_BUCKET_URI=""
+export CF_DISTRIBUTION_ID=""
+export DEPLOY_FOLDER="./public/"
 
 # Checks
-[ -z "$S3_BUCKET_NAME" ] && { echo "ERROR: S3_BUCKET_NAME is NULL"; exit 10; } 
+[ -z "$S3_BUCKET_URI" ] && { echo "ERROR: S3_BUCKET_URI is NULL"; exit 10; } 
 [ -z "$CF_DISTRIBUTION_ID" ] && echo "WARNING: CF_DISTRIBUTION_ID is NULL" || echo "INFO: CF distribution detected"
 
 # Build
 {
     hugo -D
-} || { echo "ERROR: Build failed, check hugo configurarion!"; exit 10; }
+} || { echo "ERROR: Build failed, check hugo configuration!"; exit 10; }
 
 # Deploy
 {
-    aws s3 sync ./public
-} || { echo "ERROR: Build failed, check hugo configurarion!"; exit 10; }
+    aws s3 sync $DEPLOY_FOLDER $S3_BUCKET_URI
+} || { echo "ERROR: Sync to S3 failed, check configuration"; exit 10; }
